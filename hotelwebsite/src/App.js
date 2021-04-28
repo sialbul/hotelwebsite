6b45b41ae5d07bc30d9bdf214002eba12b8e3379
Sibel <sbaslamisli@yahoo.com>
2021-04-28 10:42:10 -0400

    routes are done

--- hotelwebsite/src/App.js
+++ hotelwebsite/src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/NavBar";
 
 require("dotenv").config();
 
 function App() {
-    return <div> Test</div>;
+    return (
+        <>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/rooms/" exact component={Rooms} />
+                <Route path="/rooms/:slug" exact component={SingleRoom} />
+                <Route component={Error} />
+            </Switch>
+        </>
+    );
 }
 
 export default App;
